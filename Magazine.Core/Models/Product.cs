3a93d71ee8fb0e153568212e8b922a1f7d4d2890
Danil <dan.krut10@gmail.com>
2024-09-06 20:23:42 +0300

    class Product

---
+++ Magazine.Core/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magazine.Core.Models
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Definition { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Image {  get; set; } 
+        
+    }
+}

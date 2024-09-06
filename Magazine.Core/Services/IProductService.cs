b385e1a418e12f77a5f30e5108798629bfdd99f9
Danil <dan.krut10@gmail.com>
2024-09-06 20:25:16 +0300

    interface

---
+++ Magazine.Core/Services/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magazine.Core.Services
+{
+    internal interface IProductService
+    {
+        void Add();
+        void Remove();
+        void Edit();
+        void Search();
+    }
+}
